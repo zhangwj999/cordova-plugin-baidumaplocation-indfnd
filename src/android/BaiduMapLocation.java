58f50d44344313e913f292bc02b3ad1de486e574
zhangwj999 <zhangwenjie@inspur.com>
2019-01-16 10:15:49 +0800

    android change

--- src/android/BaiduMapLocation.java
+++ src/android/BaiduMapLocation.java
@@ -74,6 +74,7 @@ public class BaiduMapLocation extends CordovaPlugin {
                 json.put("userIndoorState", location.getUserIndoorState());
                 json.put("direction", location.getDirection());
                 json.put("locationDescribe", location.getLocationDescribe());
+				json.put("testVersion", "3");
 
                 PluginResult pluginResult;
                 if (location.getLocType() == BDLocation.TypeServerError
@@ -182,17 +183,22 @@ public class BaiduMapLocation extends CordovaPlugin {
     public LocationClientOption getDefaultLocationClientOption() {
         if (mOption == null) {
             mOption = new LocationClientOption();
-            mOption.setLocationMode(LocationMode.Hight_Accuracy);//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+            mOption.setLocationMode(LocationMode.Device_Sensors);//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
             mOption.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
-            mOption.setScanSpan(0);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+            mOption.setScanSpan(1000);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
             mOption.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
-            mOption.setOpenGps(true); // 可选，默认false,设置是否使用gps            
+            mOption.setOpenGps(true); // 可选，默认false,设置是否使用gps
             mOption.setNeedDeviceDirect(false);//可选，设置是否需要设备方向结果
-            mOption.setLocationNotify(false);//可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
+            mOption.setLocationNotify(true);//可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
             mOption.setIgnoreKillProcess(true);//可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
             mOption.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
             mOption.setIsNeedLocationPoiList(false);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
             mOption.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+
+			//设置打开自动回调位置模式，该开关打开后，期间只要定位SDK检测到位置变化就会主动回调给开发者，该模式下开发者无需再关心定位间隔是多少，定位SDK本身发现位置变化就会及时回调给开发者
+			// mOption.setOpenAutoNotifyMode();
+			//设置打开自动回调位置模式，该开关打开后，期间只要定位SDK检测到位置变化就会主动回调给开发者
+			// mOption.setOpenAutoNotifyMode(3000,1, LocationClientOption.LOC_SENSITIVITY_HIGHT);
 
             mOption.setIsNeedAltitude(false);//可选，默认false，设置定位时是否需要海拔信息，默认不需要，除基础定位版本都可用
 
